9d6d22fb3edc2ce3be07f3d142467203b91ffc89
anuragts <anuragsharma011011@gmail.com>
2025-03-24 12:58:10 +0530

    fix: comment out context

--- cookbook/playground/teams_demo.py
+++ cookbook/playground/teams_demo.py
@@ -103,7 +103,7 @@ agent_team = Team(
         table_name="financial_news_team", db_url=db_url, mode="team"
     ),
     expected_output="A good financial news report.",
-    context="List in Indian Rupees",
+    # context="List in Indian Rupees",
 )
 
 app = Playground(

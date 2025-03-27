797a4c81ed271b243f515ca1b0f5921107383926
Kaustubh <shuklakaustubh84@gmail.com>
2025-03-27 20:33:51 +0530

    Update team.py comments

--- libs/agno/agno/team/team.py
+++ libs/agno/agno/team/team.py
@@ -1970,7 +1970,7 @@ class Team:
                 videos=videos,
                 files=files,
                 stream=True,
-                stream_intermediate_steps=True,
+                stream_intermediate_steps=True, # to stream tool calls panel
                 **kwargs,
             )
 

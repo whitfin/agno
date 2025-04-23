7d2c56c3a1593afae894e420ac970c705a3dc286
kausmeows <shuklakaustubh84@gmail.com>
2025-04-24 03:00:06 +0530

    add async for pdf load

---
+++ cookbook/agent_concepts/knowledge/filters/pdf/async_user_level_filtering.py
@@ -0,0 +1,48 @@
+import asyncio
+from pathlib import Path
+
+from agno.agent import Agent
+from agno.knowledge.pdf import PDFKnowledgeBase
+from agno.vectordb.qdrant import Qdrant
+
+COLLECTION_NAME = "resume-docx-test"
+
+vector_db = Qdrant(collection=COLLECTION_NAME, url="http://localhost:6333")
+
+# Initialize the DocxKnowledgeBase
+knowledge_base = PDFKnowledgeBase(
+    vector_db=vector_db,
+)
+
+# Initialize the Agent with the knowledge_base
+agent = Agent(
+    knowledge=knowledge_base,
+    search_knowledge=True,
+)
+
+
+if __name__ == "__main__":
+    # Comment out after first run
+    asyncio.run(
+        knowledge_base.aload_pdf(
+            path=Path("data/docs"),
+            metadata={"user_id": "user_1", "document_type": "Resume"},
+            recreate=True,
+        )
+    )
+
+    asyncio.run(
+        knowledge_base.aload_pdf(
+            path=Path("data/docs"),
+            metadata={"user_id": "user_2", "document_type": "Resume"},
+        )
+    )
+
+    asyncio.run(
+        agent.aprint_response(
+            "Ask anything from doc_1",
+            knowledge_filters={"user_id": "user_1"},
+            markdown=True,
+            stream=True,
+        )
+    )

94dd2acf78ed836045b6e92a3cf76cbc4533627a
kausmeows <shuklakaustubh84@gmail.com>
2025-04-24 03:25:22 +0530

    add async text and cookbook

---
+++ cookbook/agent_concepts/knowledge/filters/text/async_user_level_filtering.py
@@ -0,0 +1,48 @@
+import asyncio
+from pathlib import Path
+
+from agno.agent import Agent
+from agno.knowledge.text import TextKnowledgeBase
+from agno.vectordb.qdrant import Qdrant
+
+COLLECTION_NAME = "resume-docx-test"
+
+vector_db = Qdrant(collection=COLLECTION_NAME, url="http://localhost:6333")
+
+knowledge_base = TextKnowledgeBase(
+    vector_db=vector_db,
+    num_documents=5,
+)
+
+# Initialize the Agent with the knowledge_base
+agent = Agent(
+    knowledge=knowledge_base,
+    search_knowledge=True,
+)
+
+
+if __name__ == "__main__":
+    # Comment out after first run
+    asyncio.run(
+        knowledge_base.aload_text(
+            path=Path("data/docs"),
+            metadata={"user_id": "user_1", "document_type": "resume"},
+            recreate=True,  # only use at the first run, True/False
+        )
+    )
+
+    asyncio.run(
+        knowledge_base.aload_text(
+            path=Path("data/docs"),
+            metadata={"user_id": "user_2", "document_type": "resume"},
+        )
+    )
+
+    asyncio.run(
+        agent.aprint_response(
+            "Ask anything about doc_1",
+            knowledge_filters={"user_id": "user_1"},
+            markdown=True,
+            stream=True,
+        )
+    )

22c5eef40e5d4be44b4bcce674ca6679c790737c
VirusDumb <soumyaray532@gmail.com>
2025-06-02 17:59:24 +0530

    media

--- cookbook/apps/discord/agent_with_media.py
+++ cookbook/apps/discord/agent_with_media.py
@@ -1,14 +1,15 @@
 from agno.agent import Agent
 from agno.app.discord.client import DiscordClient
-from agno.models.openai import OpenAIChat
+from agno.models.google import Gemini
 
-basic_agent = Agent(
-    name="Basic Agent",
-    model=OpenAIChat(id="gpt-4o"),
+media_agent = Agent(
+    name="Media Agent",
+    model=Gemini(id="gemini-2.0-flash"),
     add_history_to_messages=True,
     num_history_responses=3,
     add_datetime_to_instructions=True,
+    markdown=True,
 )
 
 if __name__ == "__main__":
-    DiscordClient(basic_agent)
+    DiscordClient(media_agent)
--- libs/agno/agno/app/discord/client.py
+++ libs/agno/agno/app/discord/client.py
@@ -3,6 +3,8 @@ from typing import Optional
 from agno.agent.agent import Agent
 from agno.media import Audio, File, Image, Video
 from agno.team.team import Team
+from agno.utils.log import log_info
+import requests
 import discord
 
 intents = discord.Intents.all()
@@ -15,14 +17,37 @@ except Exception:
 def DiscordClient(agent: Optional[Agent] = None, team: Optional[Team] = None):
     @client.event
     async def on_message(message):
-        print(message)
         if message.author == client.user:
-            print("author==client")
+            log_info(f"sent {message.content}")
         else:
+            message_image = None
+            message_video = None
+            message_audio = None
+            media_url=None
+            message_text=message.content
+            if message.attachments:
+                media=message.attachments[0]
+                media_type=media.content_type
+                media_url=media.url
+                if (media_type.startswith("image/")):
+                    message_image=media_url
+                elif(media_type.startswith("video/")):
+                    req = requests.get(media_url)
+                    video=req.content
+                    message_video=video                
+                elif(media_type.startswith("audio/")):
+                    message_audio=media_url                
+            log_info(f"processing message:{message_text} \n with media: {media_url}")
             if agent:
-                response = await agent.arun(message.content, user_id=message.author.name)
+                response = await agent.arun(message_text, user_id=message.author.name,
+                    images=[Image(url=message_image)] if message_image else None,
+                    videos=[Video(content=message_video)] if message_video else None,
+                    audio=[Audio(url=message_audio)] if message_audio else None,)
             elif team:
-                response = await team.arun(message.content, user_id=message.author.name)
+                response = await team.arun(message_text, user_id=message.author.name,
+                    images=[Image(url=message_image)] if message_image else None,
+                    videos=[Video(url=message_video)] if message_video else None,
+                    audio=[Audio(url=message_audio)] if message_audio else None,)
             await message.channel.send(response.content)
     return client.run(DISCORD_TOKEN)
     

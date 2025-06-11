c34ed80aa72b95fd9bb6bda2f4f45f1f3dfa0ecd
manuhortet <manuhortet@gmail.com>
2025-06-11 14:48:31 +0200

    feat: storage 2.0

---
+++ cookbook/storage_v2/postgres.py
@@ -0,0 +1,9 @@
+from agno.storage_v2.postgres import PostgresDb
+
+db_url = "postgresql+psycopg://ai:ai@localhost:5432/ai"
+
+db = PostgresDb(db_url=db_url, agent_sessions_table_name="agent_sessions")
+
+breakpoint()
+# agent = Agent()
+# agent.print_response("Hello")

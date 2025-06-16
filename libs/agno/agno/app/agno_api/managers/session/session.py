15b9a1a6bbbb4feafaa7a3bf934076c966da87eb
manuhortet <manuhortet@gmail.com>
2025-06-16 15:24:13 +0200

    update

--- libs/agno/agno/app/agno_api/managers/session/session.py
+++ libs/agno/agno/app/agno_api/managers/session/session.py
@@ -3,15 +3,15 @@ import logging
 from fastapi.routing import APIRouter
 
 from agno.app.agno_api.base import BaseInterface
-from agno.app.agno_api.managers.db.async_router import attach_async_routes
-from agno.app.agno_api.managers.db.sync_router import attach_sync_routes
+from agno.app.agno_api.managers.session.async_router import attach_async_routes
+from agno.app.agno_api.managers.session.sync_router import attach_sync_routes
 from agno.db.base import BaseDb
 
 logger = logging.getLogger(__name__)
 
 
-class DbManager(BaseInterface):
-    type = "db"
+class SessionManager(BaseInterface):
+    type = "session"
 
     router: APIRouter
 
@@ -20,9 +20,9 @@ class DbManager(BaseInterface):
 
     def get_router(self, use_async: bool = True) -> APIRouter:
         # Cannot be overridden
-        prefix: str = "/db"
+        prefix: str = "/session"
         version: str = "/v1"
-        self.router = APIRouter(prefix=prefix + version, tags=["Db"])
+        self.router = APIRouter(prefix=prefix + version, tags=["Session"])
 
         if use_async:
             self.router = attach_async_routes(router=self.router, db=self.db)

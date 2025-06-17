d704f79a075d98711ce5db0493e9b8186b48b21e
manuhortet <manuhortet@gmail.com>
2025-06-17 16:43:53 +0200

    working cookbooks

--- cookbook/apps/agno_api/session_manager.py
+++ cookbook/apps/agno_api/session_manager.py
@@ -1,3 +1,5 @@
+"""Simple example creating a session and using the AgnoAPI with a SessionManager to expose it"""
+
 from agno.agent import Agent
 from agno.app.agno_api import AgnoAPI
 from agno.app.agno_api.interfaces.playground import Playground
@@ -6,24 +8,28 @@ from agno.db.postgres.postgres import PostgresDb
 from agno.memory import Memory
 from agno.models.openai import OpenAIChat
 
+# Setup the database
 db_url = "postgresql+psycopg://ai:ai@localhost:5432/ai"
-
 db = PostgresDb(
     db_url=db_url,
     agent_session_table="agent_sessions",
     team_session_table="team_sessions",
     workflow_session_table="workflow_sessions",
 )
+
+# Setup the memory
 memory = Memory(db=db)
 
+# Setup the agent
 basic_agent = Agent(
     name="Basic Agent",
     model=OpenAIChat(id="gpt-4o"),
     memory=memory,
-    add_datetime_to_instructions=True,
+    enable_user_memories=True,
     markdown=True,
 )
 
+# Setup the Agno API App
 agno_client = AgnoAPI(
     name="Example App: Basic Agent",
     description="Example app for basic agent with playground capabilities",
@@ -32,8 +38,12 @@ agno_client = AgnoAPI(
     interfaces=[Playground()],
     managers=[SessionManager(db=db)],
 )
-
 app = agno_client.get_app()
 
+
 if __name__ == "__main__":
+    # Simple run to generate and record a session
+    basic_agent.print_response("What is the capital of France?")
+
+    # Run the Agno API App
     agno_client.serve(app="session_manager:app", reload=True)
--- cookbook/apps/agno_api/with_memory.py
+++ cookbook/apps/agno_api/with_memory.py
@@ -8,31 +8,40 @@ from agno.models.openai import OpenAIChat
 
 db_url = "postgresql+psycopg://ai:ai@localhost:5432/ai"
 
+# Setup the database
 db = PostgresDb(
     db_url=db_url,
     agent_session_table="agent_session",
-    user_memory_table="test_user_memories_0616",
+    user_memory_table="user_memory",
 )
 
+# Setup the memory
 memory = Memory(db=db)
 
-basic_agent = Agent(
-    name="Basic Agent",
+# Setup the agent
+agent = Agent(
+    name="Memory Agent",
     model=OpenAIChat(id="gpt-4o"),
     memory=memory,
-    add_datetime_to_instructions=True,
+    enable_user_memories=True,
     markdown=True,
 )
 
+# Setup the Agno API App
 agno_client = AgnoAPI(
     name="Example App: Basic Agent",
     description="Example app for basic agent with playground capabilities",
     app_id="basic-app",
-    agents=[basic_agent],
+    agents=[agent],
     interfaces=[Playground()],
     managers=[MemoryManager(memory=memory)],
 )
 app = agno_client.get_app()
 
+
 if __name__ == "__main__":
+    # Generate a memory
+    agent.print_response("I love astronomy, specifically the science behind nebulae")
+
+    # Run the Agno API App
     agno_client.serve(app="with_memory:app", reload=True)

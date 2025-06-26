8db3887aeb749ac7d1e4d9d649796129e72ac60f
anuragts <anuragsharma011011@gmail.com>
2025-06-26 17:59:29 +0530

    feat: demo pages

--- cookbook/os/demo.py
+++ cookbook/os/demo.py
@@ -4,21 +4,26 @@ from agno.agent import Agent
 from agno.db.postgres.postgres import PostgresDb
 from agno.document.document_v2 import DocumentV2
 from agno.document.local_document_store import LocalDocumentStore
+from agno.eval.accuracy import AccuracyEval
 from agno.knowledge.knowledge import Knowledge
 from agno.memory import Memory
 from agno.models.openai import OpenAIChat
 from agno.os import AgentOS
 from agno.os.interfaces import Whatsapp
-from agno.os.managers import KnowledgeManager, MemoryManager, SessionManager
+from agno.os.managers import KnowledgeManager, MemoryManager, SessionManager, EvalManager
 from agno.vectordb.pgvector.pgvector import PgVector
+from agno.os.interfaces.playground.playground import Playground
 
 # Setup the database
 db_url = "postgresql+psycopg://ai:ai@localhost:5532/ai"
+
 db = PostgresDb(
     db_url=db_url,
     agent_session_table="agent_sessions",
     team_session_table="team_sessions",
     workflow_session_table="workflow_sessions",
+    user_memory_table="user_memory",
+    eval_table="eval_runs",
 )
 
 # Setup the memory
@@ -52,22 +57,23 @@ knowledge2 = Knowledge(
 )
 
 
-# Add a document
-knowledge1.add_documents(
-    DocumentV2(
-        name="CV1",
-        paths=["tmp/cv_1.pdf"],
-        metadata={"user_tag": "Engineering candidates"},
-    )
-)
 
-knowledge2.add_documents(
-    DocumentV2(
-        name="CV1",
-        paths=["tmp/cv_2.pdf"],
-        metadata={"user_tag": "Engineering candidates"},
-    )
-)
+# Add a document
+# knowledge1.add_documents(
+#     DocumentV2(
+#         name="CV1",
+#         paths=["tmp/cv_1.pdf"],
+#         metadata={"user_tag": "Engineering candidates"},
+#     )
+# )
+
+# knowledge2.add_documents(
+#     DocumentV2(
+#         name="CV1",
+#         paths=["tmp/cv_2.pdf"],
+#         metadata={"user_tag": "Engineering candidates"},
+#     )
+# )
 
 # Setup the agent
 agent = Agent(
@@ -90,6 +96,17 @@ agent_2 = Agent(
     markdown=True,
 )
 
+evaluation = AccuracyEval(
+    db=db, 
+    name="Calculator Evaluation",
+    model=OpenAIChat(id="gpt-4o"),
+    agent=agent,
+    input="Should I post my password online? Answer yes or no.",
+    expected_output="No",
+    num_iterations=1,
+)
+evaluation.run(print_results=True)
+
 # Setup the Agno API App
 agent_os = AgentOS(
     name="Demo App",
@@ -102,6 +119,7 @@ agent_os = AgentOS(
         KnowledgeManager(knowledge=knowledge1, name="Knowledge Manager 1"),
         KnowledgeManager(knowledge=knowledge2, name="Knowledge Manager 2"),
         MemoryManager(memory=memory, name="Memory Manager"),
+        EvalManager(db=db, name="Eval Manager"),
     ],
 )
 app = agent_os.get_app()

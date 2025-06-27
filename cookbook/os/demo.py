b170c78c61cc18a60c67200dbb26c5620e01a378
Anurag <anuragsharma011011@gmail.com>
2025-06-27 21:02:56 +0530

    feat: update os demo file (#3691)
    ## Summary
    
    Describe key changes, mention related issues or motivation for the
    changes.
    
    (If applicable, issue number: #\_\_\_\_)
    
    ## Type of change
    
    - [ ] Bug fix
    - [ ] New feature
    - [ ] Breaking change
    - [ ] Improvement
    - [ ] Model update
    - [ ] Other:
    
    ---
    
    ## Checklist
    
    - [ ] Code complies with style guidelines
    - [ ] Ran format/validation scripts (`./scripts/format.sh` and
    `./scripts/validate.sh`)
    - [ ] Self-review completed
    - [ ] Documentation updated (comments, docstrings)
    - [ ] Examples and guides: Relevant cookbook examples have been included
    or updated (if applicable)
    - [ ] Tested in clean environment
    - [ ] Tests added/updated (if applicable)
    
    ---
    
    ## Additional Notes
    
    Add any important context (deployment instructions, screenshots,
    security considerations, etc.)

--- cookbook/os/demo.py
+++ cookbook/os/demo.py
@@ -9,7 +9,8 @@ from agno.memory import Memory
 from agno.models.openai import OpenAIChat
 from agno.os import AgentOS
 from agno.os.interfaces import Whatsapp
-from agno.os.managers import KnowledgeManager, MemoryManager, SessionManager
+from agno.os.managers import EvalManager, KnowledgeManager, MemoryManager, SessionManager
+from agno.eval.accuracy import AccuracyEval
 from agno.vectordb.pgvector.pgvector import PgVector
 
 # Setup the database
@@ -19,6 +20,8 @@ db = PostgresDb(
     agent_session_table="agent_sessions",
     team_session_table="team_sessions",
     workflow_session_table="workflow_sessions",
+    user_memory_table="user_memory",
+    eval_table="eval_runs",
 )
 
 # Setup the memory
@@ -30,10 +33,21 @@ document_store = LocalDocumentStore(
     storage_path="tmp/documents",
 )
 
-vector_store = PgVector(
+vector_store_1 = PgVector(
     table_name="pdf_documents",
-    # Can inspect database via psql e.g. "psql -h localhost -p 5432 -U ai -d ai"
+    # Can inspect database via psql e.g. "psql -h localhost -p 5532 -U ai -d ai"
     db_url=db_url,
+)
+
+vector_store_2 = PgVector(
+    table_name="pdf_documents_2",
+    # Can inspect database via psql e.g. "psql -h localhost -p 5532 -U ai -d ai"
+    db_url=db_url,
+)
+
+document_db = PostgresDb(
+    db_url=db_url,
+    knowledge_table="knowledge_documents",
 )
 
 # Create knowledge base
@@ -41,33 +55,35 @@ knowledge1 = Knowledge(
     name="My Knowledge Base",
     description="A simple knowledge base",
     document_store=document_store,
-    vector_store=vector_store,
+    documents_db=document_db,
+    vector_store=vector_store_1,
 )
 
 knowledge2 = Knowledge(
     name="My Knowledge Base 2",
     description="A simple knowledge base 2",
     document_store=document_store,
-    vector_store=vector_store,
+    documents_db=document_db,
+    vector_store=vector_store_2,
 )
 
 
 # Add a document
-knowledge1.add_documents(
-    DocumentV2(
-        name="CV1",
-        paths=["tmp/cv_1.pdf"],
-        metadata={"user_tag": "Engineering candidates"},
-    )
-)
+# knowledge1.add_documents(
+#     DocumentV2(
+#         name="CV1",
+#         paths=["tmp/cv_1.pdf"],
+#         metadata={"user_tag": "Engineering candidates"},
+#     )
+# )
 
-knowledge2.add_documents(
-    DocumentV2(
-        name="CV1",
-        paths=["tmp/cv_2.pdf"],
-        metadata={"user_tag": "Engineering candidates"},
-    )
-)
+# knowledge2.add_documents(
+#     DocumentV2(
+#         name="CV1",
+#         paths=["tmp/cv_2.pdf"],
+#         metadata={"user_tag": "Engineering candidates"},
+#     )
+# )
 
 # Setup the agent
 agent = Agent(
@@ -90,6 +106,17 @@ agent_2 = Agent(
     markdown=True,
 )
 
+evaluation = AccuracyEval(
+    db=db, 
+    name="Calculator Evaluation",
+    model=OpenAIChat(id="gpt-4o"),
+    agent=agent,
+    input="Should I post my password online? Answer yes or no.",
+    expected_output="No",
+    num_iterations=1,
+)
+
+evaluation.run(print_results=True)  # Comment this to prevent the eval from running
 # Setup the Agno API App
 agent_os = AgentOS(
     name="Demo App",
@@ -102,6 +129,7 @@ agent_os = AgentOS(
         KnowledgeManager(knowledge=knowledge1, name="Knowledge Manager 1"),
         KnowledgeManager(knowledge=knowledge2, name="Knowledge Manager 2"),
         MemoryManager(memory=memory, name="Memory Manager"),
+        EvalManager(db=db, name="Eval Manager"),
     ],
 )
 app = agent_os.get_app()

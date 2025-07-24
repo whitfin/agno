53220443e4d5478b5b99d4d931593c14c2a48991
manu <manuhortet@gmail.com>
2025-07-24 16:41:49 +0200

    feat: api layer adjustments for new memory and db dx (#3947)

--- cookbook/os/basic.py
+++ cookbook/os/basic.py
@@ -1,10 +1,11 @@
 from agno.agent import Agent
 from agno.db.postgres import PostgresDb
-from agno.memory import Memory
 from agno.models.openai import OpenAIChat
 from agno.os import AgentOS
 from agno.os.interfaces.slack import Slack
 from agno.os.interfaces.whatsapp import Whatsapp
+from agno.team import Team
+from agno.tools.calculator import CalculatorTools
 
 db_url = "postgresql+psycopg://ai:ai@localhost:5532/ai"
 
@@ -15,20 +16,25 @@ db = PostgresDb(
     user_memory_table="user_memory",
 )
 
-# Setup the memory
-memory = Memory(db=db)
-
 
 basic_agent = Agent(
-    name="Basic Agent",
+    name="Calculator Agent",
     model=OpenAIChat(id="gpt-4o"),
-    memory=memory,
+    tools=[CalculatorTools(enable_all=True)],
+    db=db,
     enable_user_memories=True,
     enable_session_summaries=True,
     add_history_to_messages=True,
     num_history_runs=3,
     add_datetime_to_instructions=True,
     markdown=True,
+)
+basic_team = Team(
+    name="team",
+    team_id="1",
+    members=[basic_agent],
+    db=db,
+    show_tool_calls=True,
 )
 
 agent_os = AgentOS(
@@ -37,6 +43,7 @@ agent_os = AgentOS(
     agents=[
         basic_agent,
     ],
+    teams=[basic_team],
     interfaces=[Whatsapp(agent=basic_agent), Slack(agent=basic_agent)],
 )
 app = agent_os.get_app()

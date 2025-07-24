02abdcdc4564991b0920020dc6f6ff26091c5052
Mustafa-Esoofally <coolmusta@gmail.com>
2025-07-24 13:17:04 -0400

    revert formatting changes

--- cookbook/agent_concepts/knowledge/vector_db/weaviate_db/weaviate_db.py
+++ cookbook/agent_concepts/knowledge/vector_db/weaviate_db/weaviate_db.py
@@ -6,7 +6,11 @@ from agno.vectordb.weaviate.index import VectorIndex, Distance
 
 vector_db = Weaviate(
     collection="vectors",
-	@@ -14,7 +14,7 @@
+    search_type=SearchType.vector,
+    vector_index=VectorIndex.HNSW,
+    distance=Distance.COSINE,
+    local=False,  # Set to True if using Weaviate locally
+)
 
 # Create Knowledge Instance with Weaviate
 knowledge = Knowledge(
@@ -14,3 +18,18 @@ knowledge = Knowledge(
     description="Agno 2.0 Knowledge Implementation with Weaviate",
     vector_store=vector_db,
 )
+
+knowledge.add_content(
+    name="Recipes",
+    url="https://agno-public.s3.amazonaws.com/recipes/ThaiRecipes.pdf",
+    metadata={"doc_type": "recipe_book"},
+)
+
+# Create and use the agent
+agent = Agent(knowledge=knowledge)
+agent.print_response("List down the ingredients to make Massaman Gai", markdown=True)
+
+# Delete operations
+vector_db.delete_by_name("Recipes")
+# or
+vector_db.delete_by_metadata({"doc_type": "recipe_book"})

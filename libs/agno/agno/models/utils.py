73fae0f44fbc090b959200437d424e5fa8246deb
Dirk Brand <dirkbrnd@gmail.com>
2025-07-29 14:09:20 +0200

    Fix model imports

--- libs/agno/agno/models/utils.py
+++ libs/agno/agno/models/utils.py
@@ -1,17 +1,17 @@
-from agno.models.anthropic.claude import Claude
-from agno.models.base import Model
-from agno.models.google import Gemini
-from agno.models.openai import OpenAIChat
 
+from agno.models.base import Model
 
 # TODO: add all supported models
 def get_model(model_id: str, model_provider: str) -> Model:
     """Return the right Agno model instance given a pair of model provider and id"""
     if model_provider == "openai":
+        from agno.models.openai import OpenAIChat
         return OpenAIChat(id=model_id)
     elif model_provider == "anthropic":
+        from agno.models.anthropic import Claude
         return Claude(id=model_id)
     elif model_provider == "gemini":
+        from agno.models.google import Gemini   
         return Gemini(id=model_id)
     else:
         raise ValueError(f"Model provider {model_provider} not supported")

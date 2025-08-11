6b77da0d9b2b25879446fd6b0405b7c445c233d4
manuhortet <manuhortet@gmail.com>
2025-08-11 15:16:34 +0200

    chore: add workflow to demo

--- cookbook_v2/demo/agent_os.py
+++ cookbook_v2/demo/agent_os.py
@@ -6,12 +6,14 @@ from agents.research_agent import research_agent
 from agno.os import AgentOS
 from agno.os.interfaces.whatsapp import Whatsapp
 from teams.multi_language_team import multi_language_team
+from workflows.article_creation import article_workflow
 
 # ************* AgentOS *************
 agent_os = AgentOS(
     os_id="agentos-demo",
     agents=[agno_agent, finance_agent, research_agent],
     teams=[multi_language_team],
+    workflows=[article_workflow],
     interfaces=[Whatsapp(agent=agno_agent)],
 )
 app = agent_os.get_app()
